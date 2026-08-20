6c02774e971b1947f136d5c7bf6d2505baf95638
frank-2077 <siruhe666@gmail.com>
2026-08-20 14:26:13 +0000

    feat(distributed): add ROCm transport-only deterministic collectives

--- rl_engine/distributed/__init__.py
+++ rl_engine/distributed/__init__.py
@@ -2,5 +2,15 @@
 # Copyright (c) 2026 RL-Kernel Contributors
 
 from rl_engine.distributed.collectives import DeterministicCollective
+from rl_engine.distributed.transport_collectives import (
+    RCCLDeterministicCollective,
+    TorchDistributedDeterministicCollective,
+    create_deterministic_collective,
+)
 
-__all__ = ["DeterministicCollective"]
+__all__ = [
+    "DeterministicCollective",
+    "RCCLDeterministicCollective",
+    "TorchDistributedDeterministicCollective",
+    "create_deterministic_collective",
+]
